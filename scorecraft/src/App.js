528b5cebca5b2f0f10724bd6492412b1502d069d
mohana0103 <mkaruma1@asu.edu>
2024-02-03 18:12:46 -0700

    Task 3: Initialize a new React application using Create React App or a similar tool.

---
+++ scorecraft/src/App.js
@@ -0,0 +1,16 @@
+import LoginPage from './LoginPage';
+import './App.css';
+import React from 'react';
+;
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Score Craft</h1> {/* Move "Score Craft" heading outside LoginPage */}
+      <LoginPage />
+    
+  </div>
+  );
+}
+
+export default App;

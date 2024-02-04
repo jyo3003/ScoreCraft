7753ab8c0f57c6264f401d2a06d13d2d8ff9b5e5
manideepnalluri12 <mnallur2@asu.edu>
2024-02-04 16:33:55 -0700

    Adding Buttons for upload and function for verifying correct file

--- scorecraft/src/App.js
+++ scorecraft/src/App.js
@@ -1,4 +1,5 @@
 import LoginPage from './LoginPage';
+import FileUploadPage from './FileUploadPage';
 import './App.css';
 import React from 'react';
 ;
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Score Craft</h1> {/* Move "Score Craft" heading outside LoginPage */}
       <LoginPage />
+      <FileUploadPage/>
     
   </div>
   );

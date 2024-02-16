8623f41eae69293a95b92025fac68bf3b2f95558
smanda38 <112658309+smanda38@users.noreply.github.com>
2024-02-15 21:10:30 -0700

    #37 In the React frontend, set up functions or components to handle API requests to the Spring Boot backend.

---
+++ scorecraft/src/api.js
@@ -0,0 +1,21 @@
+// api.js
+
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3300/api/';
+
+export const login = (email, password) => {
+  return axios.post(API_URL + 'login', { email, password })
+    .then(response => response.data)
+    .catch(error => {
+      throw error.response.data;
+    });
+};
+
+export const register = (email, password) => {
+  return axios.post(API_URL + 'register', { email, password })
+    .then(response => response.data)
+    .catch(error => {
+      throw error.response.data;
+    });
+};

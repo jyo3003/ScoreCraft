a85b6bfb5a091120f7bd8613f3b81f2de41b8ecb
manideepnalluri12 <mnallur2@asu.edu>
2024-02-25 22:44:02 -0600

    Integrate the grading criteria Repository

---
+++ scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/service/GradingRepository.java
@@ -0,0 +1,13 @@
+package com.SER517.scorecraft_backend.service;
+
+import com.SER517.scorecraft_backend.model.GradingCriteria;
+
+import java.util.List;
+
+public class GradingRepository {
+    public List<GradingCriteria> findAll() {
+    }
+
+    public GradingCriteria save(GradingCriteria gradingCriteria) {
+    }
+}

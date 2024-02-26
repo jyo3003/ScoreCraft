3327be0a6a3cb516d8e6246ba1fe28b9856e0ac7
jyo3003 <112651663+jyo3003@users.noreply.github.com>
2024-02-25 19:15:03 -0700

    Updating user service and controller

--- scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/service/UserService.java
+++ scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/service/UserService.java
@@ -1,5 +1,34 @@
 package com.SER517.scorecraft_backend.service;
 
+import com.SER517.scorecraft_backend.model.User;
+import com.SER517.scorecraft_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public boolean authenticateUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            return passwordEncoder.matches(password, user.getPassword());
+        }
+        return false;
+    }
 }

158288dfe3917bb139c4fd458f0cc8b2a25b5d71
jyo3003 <112651663+jyo3003@users.noreply.github.com>
2024-02-25 19:12:27 -0700

    Update UserRepository.java

--- scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/repository/UserRepository.java
+++ scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
     boolean existsByEmail(String email);
 }

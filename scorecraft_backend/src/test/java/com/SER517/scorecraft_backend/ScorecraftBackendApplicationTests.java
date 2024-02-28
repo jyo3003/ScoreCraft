8ea1178b2add42db30c1ffe209c344ad92535f96
manideepnalluri12 <mnallur2@asu.edu>
2024-02-28 14:58:50 -0600

    reverting the last commit

---
+++ scorecraft_backend/src/test/java/com/SER517/scorecraft_backend/ScorecraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SER517.scorecraft_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorecraftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

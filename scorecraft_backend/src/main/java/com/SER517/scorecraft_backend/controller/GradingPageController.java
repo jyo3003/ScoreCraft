ad2090344f36ed236dc7c809272477e560654b8b
Jyothirmai Kantipudi <jo@Jyothirmais-MacBook-Air.local>
2024-03-22 08:34:44 -0700

    minor chnage

--- scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/controller/GradingPageController.java
+++ scorecraft_backend/src/main/java/com/SER517/scorecraft_backend/controller/GradingPageController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -26,6 +27,7 @@ public class GradingPageController {
             return ResponseEntity.notFound().build();
         }
     }
+
    
     
     @PostMapping("/submitGrades")

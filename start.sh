cc3f4c287832b7cdc8681655b3af02ce32baa847
Harsha-Vardhan-99 <hbodepud@asu.edu>
2024-04-21 13:04:21 -0700

    Fixed issues in application start script

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Change directory to your Spring Boot backend
-cd scorecraft_backend/src/main/java/com/SER517/scorecraft_backend
+cd scorecraft_backend
 
 # Start Spring Boot backend
-mvn clean package spring-boot:run
+mvn clean package spring-boot:run &
 
 # Wait for a few seconds 
 sleep 15
 
 
-cd /Users/mohanakarumanchi/Desktop/SER517_Team16/scorecraft
+cd ..
+cd scorecraft
 
 # Install dependencies and start frontend
 npm install
